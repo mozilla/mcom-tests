16ce5cea534cb9dfd766806f01a3dc723e64093b
retornam <etornam.raymond@gmail.com>
2011-06-02 16:54:12 -0700

    adding test for 404 errors

---
+++ test_404.py
@@ -0,0 +1,13 @@
+import urllib
+
+
+def testErrors():
+	url = 'http://www.mozilla.com/en-US/abck'
+	page = urllib.urlopen(url)
+	if  page.code == 404:
+	    print 'passed'
+	else:
+	    print "%s returned %d" %(url,page.code)
+	
+	
+testErrors()
