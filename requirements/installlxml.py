ee9fdc8cba9264cd2d40cd2c26cc2a0d3fb6ec48
Dave Hunt <dave.hunt@gmail.com>
2011-09-01 22:58:12 +0100

    Removed dependancy on lxml.

--- requirements/installlxml.py
+++
@@ -1,8 +0,0 @@
-cmd="STATIC_DEPS=true easy_install 'lxml>=2.2alpha1'"
-try:
-   import lxml
-   print 'libxml already installed'
-except ImportError, e:
-    import commands
-    commands.getoutput(cmd)
-   
