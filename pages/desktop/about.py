00e481ee2615ba174b8e9aa9def9911c9f3a5cb5
retornam <etornam.raymond@gmail.com>
2012-06-06 17:40:02 -0700

    Fixing broken elements for test_about

--- pages/desktop/about.py
+++ pages/desktop/about.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -12,10 +13,10 @@ class AboutPage(Base):
     def go_to_page(self):
         self.open('/about/')
 
-    _mission_header_link = (By.CSS_SELECTOR, '#nav-main > ol > li:nth-child(1) > a')
-    _about_header_link = (By.CSS_SELECTOR, '#nav-main > ol > li:nth-child(2) > a')
-    _projects_header_link = (By.CSS_SELECTOR, '#nav-main > ol > li:nth-child(3) > a')
-    _get_involved_link = (By.CSS_SELECTOR, '#nav-main > ol > li:nth-child(4) > a')
+    _mission_header_link = (By.CSS_SELECTOR, '#nav-main > ul > li:nth-child(1) > a')
+    _about_header_link = (By.CSS_SELECTOR, '#nav-main > ul > li:nth-child(2) > a')
+    _projects_header_link = (By.CSS_SELECTOR, '#nav-main > ul > li:nth-child(3) > a')
+    _get_involved_header_link = (By.CSS_SELECTOR, '#nav-main > ul > li:nth-child(4) > a')
     _get_to_know_mozilla_header = (By.CSS_SELECTOR, '.large.center')
     _career_center_link = (By.CSS_SELECTOR, '.links:nth-of-type(1) > li:nth-of-type(1) > h4 > a')
     _press_center_link = (By.CSS_SELECTOR, '.links:nth-of-type(1) > li:nth-of-type(2) > h4 > a')
@@ -23,7 +24,7 @@ class AboutPage(Base):
     _privacy_center_link = (By.CSS_SELECTOR, '.links:nth-of-type(1) > li:nth-of-type(4) > h4 > a')
     _forums_link = (By.CSS_SELECTOR, '.links:nth-of-type(1) > li:nth-of-type(5) > h4 > a')
     _governance_link = (By.CSS_SELECTOR, '.links:nth-of-type(2) > li:nth-of-type(5) > h4 > a')
-    _get_invloved_link = (By.CSS_SELECTOR, '.links:nth-of-type(2) > li:nth-of-type(4) > h4 > a')
+    _get_involved_link = (By.CSS_SELECTOR, '.links:nth-of-type(2) > li:nth-of-type(4) > h4 > a')
     _locations_link = (By.CSS_SELECTOR, '.links:nth-of-type(2) > li:nth-of-type(3) > h4 > a')
     _partnerships_link = (By.CSS_SELECTOR, '.links:nth-of-type(2) > li:nth-of-type(2) > h4 > a')
     _brand_toolkit_link = (By.CSS_SELECTOR, '.links:nth-of-type(2) > li:nth-of-type(1) > h4 > a')
@@ -36,7 +37,7 @@ class AboutPage(Base):
                self.is_element_present(*self._privacy_center_link) and \
                self.is_element_present(*self._forums_link) and \
                self.is_element_present(*self._governance_link) and \
-               self.is_element_present(*self._get_invloved_link) and \
+               self.is_element_present(*self._get_involved_header_link) and \
                self.is_element_present(*self._locations_link) and \
                self.is_element_present(*self._partnerships_link) and \
                self.is_element_present(*self._brand_toolkit_link)
@@ -46,7 +47,7 @@ class AboutPage(Base):
         return self.is_element_present(*self._mission_header_link) and \
                self.is_element_present(*self._about_header_link) and \
                self.is_element_present(*self._projects_header_link) and \
-               self.is_element_present(*self._get_invloved_link)
+               self.is_element_present(*self._get_involved_link)
 
     @property
     def is_know_mozilla_header_present(self):
