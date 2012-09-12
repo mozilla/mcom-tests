b1b087991e34d3de966d9c2ba507017bd5b336f3
Bob Silverberg <bob.silverberg@gmail.com>
2012-09-12 08:03:35 -0400

    Refactored mission page to use hrefs in locators instead of nth-of-type as per issue #78

--- pages/desktop/mission.py
+++ pages/desktop/mission.py
@@ -10,26 +10,36 @@ from selenium.webdriver.common.by import By
 
 class Mission(Base):
 
-    _contribute_midpage_link = (By.CSS_SELECTOR, 'ul.links > li:nth-of-type(1) > h4 > a')
-    _history_midpage_link = (By.CSS_SELECTOR, 'ul.links > li:nth-of-type(2) > h4 > a')
-    _forums_midpage_link = (By.CSS_SELECTOR, 'ul.links > li:nth-of-type(3) > h4 > a')
-    _governance_midpage_link = (By.CSS_SELECTOR, 'ul.links > li:nth-of-type(4) > h4 > a')
-    _manifesto_midpage_link = (By.CSS_SELECTOR, '#main-content > div > p:nth-of-type(3) > a')
-    _video = (By.TAG_NAME, 'video')
-    _video_overlay = (By.CSS_SELECTOR, '.mozilla-video-control-overlay')
+    _midpage_link_locator_expression = 'a[href$="%s"]'
+    _video_locator = (By.TAG_NAME, 'video')
+    _video_overlay_locator = (By.CSS_SELECTOR, '.mozilla-video-control-overlay')
+    _video_srcs_locator = (By.CSS_SELECTOR, 'video source')
+
+    midpage_links_list = [
+        '/about/manifesto.html',
+        '/contribute/',
+        '/about/history.html',
+        '/about/forums/',
+        '/about/governance.html'
+    ]
+
+    def midpage_link_href(self, url_suffix):
+        return self.selenium.find_element(
+            By.CSS_SELECTOR, self._midpage_link_locator_expression % url_suffix
+        ).get_attribute('href')
 
     def go_to_page(self):
         self.open('/mission/')
 
     @property
-    def are_midpage_links_visible(self):
-        return self.is_element_visible(*self._contribute_midpage_link) and \
-        self.is_element_visible(*self._forums_midpage_link) and \
-        self.is_element_visible(*self._governance_midpage_link) and \
-        self.is_element_visible(*self._history_midpage_link) and\
-        self.is_element_visible(*self._manifesto_midpage_link)
+    def is_video_visible(self):
+        return self.is_element_visible(*self._video_locator) and \
+            self.is_element_visible(*self._video_overlay_locator)
 
     @property
-    def is_video_visible(self):
-        return self.is_element_visible(*self._video) and\
-               self.is_element_visible(*self._video_overlay)
+    def video_srcs(self):
+        srcs = []
+        sources = self.selenium.find_elements(*self._video_srcs_locator)
+        for source in sources:
+            srcs.append(source.get_attribute('src'))
+        return srcs
