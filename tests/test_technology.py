274c102a10529676590ffe3a7cac85eca21a1266
Bob Silverberg <bob.silverberg@gmail.com>
2012-10-01 12:31:53 -0400

    Updated all the tests in test_technology.py to use soft fails

--- tests/test_technology.py
+++ tests/test_technology.py
@@ -45,13 +45,36 @@ class TestTechnologyPage:
         Assert.equal(0, len(bad_urls), '%s bad urls found: ' % len(bad_urls) + ', '.join(bad_urls))
 
     @pytest.mark.nondestructive
-    def test_more_info_links_are_correct(self, mozwebqa):
+    def test_more_info_links_are_visible(self, mozwebqa):
         technology_page = Technology(mozwebqa)
         technology_page.go_to_page()
+        bad_links = []
+        for link in technology_page.more_info_links_list:
+            if not technology_page.is_element_visible(*link.get('locator')):
+                bad_links.append('The link at %s is not visible' % link.get('locator')[1:])
+        Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
+
+    @pytest.mark.nondestructive
+    def test_more_info_link_destinations_are_correct(self, mozwebqa):
+        technology_page = Technology(mozwebqa)
+        technology_page.go_to_page()
+        bad_links = []
         for link in technology_page.more_info_links_list:
             url = technology_page.link_destination(link.get('locator'))
-            Assert.true(url.endswith(link.get('url_suffix')), '%s does not end with %s' % (url, link.get('url_suffix')))
-            Assert.true(technology_page.is_valid_link(url), '%s is not a valid url' % url)
+            if not url.endswith(link.get('url_suffix')):
+                bad_links.append('%s does not end with %s' % (url, link.get('url_suffix')))
+        Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
+
+    @pytest.mark.nondestructive
+    def test_more_info_link_urls_are_valid(self, mozwebqa):
+        technology_page = Technology(mozwebqa)
+        technology_page.go_to_page()
+        bad_urls = []
+        for link in technology_page.more_info_links_list:
+            url = technology_page.link_destination(link.get('locator'))
+            if not technology_page.is_valid_link(url):
+                bad_urls.append('%s is not a valid url' % url)
+        Assert.equal(0, len(bad_urls), '%s bad urls found: ' % len(bad_urls) + ', '.join(bad_urls))
 
     @pytest.mark.nondestructive
     def test_footer_section(self, mozwebqa):
@@ -61,9 +84,12 @@ class TestTechnologyPage:
                         technology_page.footer.footer_logo_destination)
         Assert.contains(technology_page.footer.expected_footer_logo_img,
                         technology_page.footer.footer_logo_img)
+        bad_links = []
         for link in Technology.Footer.footer_links_list:
             url = technology_page.link_destination(link.get('locator'))
-            Assert.true(url.endswith(link.get('url_suffix')), '%s does not end with %s' % (url, link.get('url_suffix')))
+            if not url.endswith(link.get('url_suffix')):
+                bad_links.append('%s does not end with %s' % (url, link.get('url_suffix')))
+        Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
 
     @pytest.mark.nondestructive
     def test_tabzilla_links_are_correct(self, mozwebqa):
@@ -71,9 +97,12 @@ class TestTechnologyPage:
         technology_page.go_to_page()
         Assert.true(technology_page.header.is_tabzilla_panel_visible)
         technology_page.header.toggle_tabzilla_dropdown()
+        bad_links = []
         for link in Technology.Header.tabzilla_links_list:
             url = technology_page.link_destination(link.get('locator'))
-            Assert.true(url.endswith(link.get('url_suffix')), '%s does not end with %s' % (url, link.get('url_suffix')))
+            if not url.endswith(link.get('url_suffix')):
+                bad_links.append('%s does not end with %s' % (url, link.get('url_suffix')))
+        Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
 
     @pytest.mark.nondestructive
     def test_download_button_section(self, mozwebqa):
