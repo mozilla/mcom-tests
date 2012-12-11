f573df1f04a3b2c6672719f9f49d7aa8f1e4301b
Bob Silverberg <bob.silverberg@gmail.com>
2012-12-11 14:24:37 -0500

    Remove Selenium from test for valid links on notes page

--- pages/desktop/notes.py
+++ pages/desktop/notes.py
@@ -10,21 +10,15 @@ from selenium.webdriver.common.by import By
 class Notes(Base):
 
     _firefox_notes_header_locator = (By.CSS_SELECTOR, '#main-feature > h2')
+    _notes_page_url = '/firefox/notes/'
 
     def go_to_page(self):
-        self.open('/firefox/notes/')
+        self.open(self._notes_page_url)
+
+    @property
+    def notes_page_url(self):
+        return self._notes_page_url
 
     @property
     def firefox_notes_header_text(self):
         return self.selenium.find_element(*self._firefox_notes_header_locator).text
-
-    @property
-    def all_links(self):
-        current_url = self.selenium.current_url
-        urls = []
-        links = self.selenium.find_elements(By.TAG_NAME, 'a')
-        for link in links:
-            href = link.get_attribute('href')
-            if len(href) > 0 and href not in urls and current_url not in href:
-                urls.append(href)
-        return urls
--- tests/test_notes.py
+++ tests/test_notes.py
@@ -3,20 +3,37 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import pytest
+import requests
+from BeautifulSoup import BeautifulStoneSoup
 from unittestzero import Assert
 from pages.desktop.notes import Notes
-import pytest
 
 
 class TestNotes:
 
     @pytest.mark.nondestructive
-    def test_all_links(self, mozwebqa):
+    def test_that_notes_page_is_reachable(self, mozwebqa):
         notes_page = Notes(mozwebqa)
         notes_page.go_to_page()
         Assert.contains("Notes", notes_page.firefox_notes_header_text)
+
+    @pytest.mark.skip_selenium
+    @pytest.mark.nondestructive
+    def test_that_all_links_are_valid(self, mozwebqa):
+        notes_page = Notes(mozwebqa)
+        url = mozwebqa.base_url + notes_page.notes_page_url
+        page_response = requests.get(url)
+        html = BeautifulStoneSoup(page_response.content)
         bad_urls = []
-        for url in notes_page.all_links:
+        links = html.findAll('a')
+        for link in links:
+            url = self.make_absolute(link['href'], mozwebqa.base_url)
             if not notes_page.is_valid_link(url):
                 bad_urls.append('%s is not a valid url' % url)
         Assert.equal(0, len(bad_urls), '%s bad urls found: ' % len(bad_urls) + ', '.join(bad_urls))
+
+    def make_absolute(self, url, base_url):
+        if url.startswith('http'):
+            return url
+        return base_url + url
