80a109d6a44d26c90b098f4c30129096a1c8943e
Bob Silverberg <bob.silverberg@gmail.com>
2012-12-11 13:46:25 -0500

    Remove Selenium from localization downloads test

--- tests/test_localization.py
+++ tests/test_localization.py
@@ -3,31 +3,33 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from pages.desktop.localization import Localization
-from unittestzero import Assert
-import requests
 import pytest
+import requests
+from BeautifulSoup import BeautifulStoneSoup
+from unittestzero import Assert
 
 
+@pytest.mark.skip_selenium
+@pytest.mark.nondestructive
 class TestLocalizations:
 
-    @pytest.mark.nondestructive
     def test_that_download_links_return_302(self, mozwebqa):
         '''
             Check that download links return a status 302.
         '''
-        localizations_page = Localization(mozwebqa)
-        localizations_page.go_to_page()
-        languages = localizations_page.get_released_languages + localizations_page.get_beta_languages
+        url = "%s/firefox/all.html" % mozwebqa.base_url
+        page_response = requests.get(url)
+        html = BeautifulStoneSoup(page_response.content)
         bad_links = []
-        for language in languages:
-            for (url, os_name) in ((language['windows_url'], "Windows"),
-                                   (language['osx_url'], "OSX"),
-                                   (language['linux_url'], "Linux")):
+        language_rows = html.findAll('tr', id=True)
+        for language in language_rows:
+            links = language.findAll('a')
+            for link in links:
+                url = link['href']
                 response = requests.head(url, allow_redirects=False)
                 status = response.status_code
                 if status != 302:
                     bad_links.append("Lang '%s' %s link: status %s"
-                                            % (language['id'], os_name, status))
+                                     % (language['id'], link['class'], status))
         Assert.equal(0, len(bad_links),
-                        'Expected status code 302.  ' + ",  ".join(bad_links))
+                     "Expected status code 302.  " + ",  ".join(bad_links))
