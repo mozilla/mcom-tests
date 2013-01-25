5fa0422e1fbed5cc6be38815b146e75d38e2d929
Andrei Hutusoru <hutusoru.andrei@gmail.com>
2013-01-25 10:01:37 +0200

    Contribute_page coverage

--- tests/test_contribute.py
+++ tests/test_contribute.py
@@ -13,6 +13,72 @@ from unittestzero import Assert
 class TestContribute:
 
     @pytest.mark.nondestructive
+    def test_images_are_visible(self, mozwebqa):
+        contribute_page = Contribute(mozwebqa)
+        contribute_page.go_to_page()
+        bad_images = []
+        for image in contribute_page.images_list:
+            if not contribute_page.is_element_visible(*image.get('locator')):
+                bad_images.append('The image at %s is not visible' % image.get('locator')[1:])
+        Assert.equal(0, len(bad_images), '%s bad images found: ' % len(bad_images) + ', '.join(bad_images))
+
+    @pytest.mark.nondestructive
+    def test_image_srcs_are_correct(self, mozwebqa):
+        contribute_page = Contribute(mozwebqa)
+        contribute_page.go_to_page()
+        bad_images = []
+        for image in contribute_page.images_list:
+            src = contribute_page.image_source(image.get('locator'))
+            if not src.endswith(image.get('img_name_suffix')):
+                bad_images.append('%s does not end with %s' % (src, image.get('img_name_suffix')))
+        Assert.equal(0, len(bad_images), '%s bad images found: ' % len(bad_images) + ', '.join(bad_images))
+
+    @pytest.mark.nondestructive
+    def test_images_srcs_are_valid(self, mozwebqa):
+        contribute_page = Contribute(mozwebqa)
+        contribute_page.go_to_page()
+        bad_urls = []
+        for image in contribute_page.images_list:
+            url = contribute_page.image_source(image.get('locator'))
+            response_code = contribute_page.get_response_code(url)
+            if response_code != requests.codes.ok:
+                bad_urls.append('%s is not a valid url - status code: %s.' % (url, response_code))
+        Assert.equal(0, len(bad_urls), '%s bad urls found: ' % len(bad_urls) + ', '.join(bad_urls))
+
+    @pytest.mark.nondestructive
+    def test_tools_links_are_visible(self, mozwebqa):
+        contribute_page = Contribute(mozwebqa)
+        contribute_page.go_to_page()
+        bad_links = []
+        for link in contribute_page.details_links:
+            if not contribute_page.is_element_visible(*link.get('locator')):
+                bad_links.append('The link at %s is not visible' % link.get('locator')[1:])
+        Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
+
+    @pytest.mark.nondestructive
+    def test_tools_link_destinations_are_correct(self, mozwebqa):
+        contribute_page = Contribute(mozwebqa)
+        contribute_page.go_to_page()
+        bad_links = []
+        for link in contribute_page.details_links:
+            url = contribute_page.link_destination(link.get('locator'))
+            if not url.endswith(link.get('url_suffix')):
+                bad_links.append('%s does not end with %s' % (url, link.get('url_suffix')))
+        Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
+
+    @pytest.mark.nondestructive
+    def test_tools_link_urls_are_valid(self, mozwebqa):
+        contribute_page = Contribute(mozwebqa)
+        contribute_page.go_to_page()
+        bad_urls = []
+        for link in contribute_page.details_links:
+            url = contribute_page.link_destination(link.get('locator'))
+            response_code = contribute_page.get_response_code(url)
+            if response_code != requests.codes.ok:
+                bad_urls.append('%s is not a valid url - status code: %s.' % (url, response_code))
+        Assert.equal(0, len(bad_urls), '%s bad urls found: ' % len(bad_urls) + ', '.join(bad_urls))
+
+    @pytest.mark.nondestructive
     def test_footer_section(self, mozwebqa):
         contribute_page = Contribute(mozwebqa)
         contribute_page.go_to_page()
