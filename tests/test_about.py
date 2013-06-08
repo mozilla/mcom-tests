4795eb6701fbb73a5c6b3ec3d92baa14edf71d26
retornam <etornam.raymond@gmail.com>
2013-06-07 17:00:56 -0700

    changing endswith to find in test_tabzilla_links_are_correct

--- tests/test_about.py
+++ tests/test_about.py
@@ -52,7 +52,7 @@ class TestAboutPage:
         bad_links = []
         for link in AboutPage.Header.tabzilla_links_list:
             url = about_page.link_destination(link.get('locator'))
-            if not url.endswith(link.get('url_suffix')):
+            if url.find(link.get('url_suffix')) < 1:
                 bad_links.append('%s does not end with %s' % (url, link.get('url_suffix')))
         Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
 
--- tests/test_apps.py
+++ tests/test_apps.py
@@ -23,7 +23,7 @@ class TestApps:
         bad_links = []
         for link in Apps.Footer.footer_links_list:
             url = apps_page.link_destination(link.get('locator'))
-            if not url.endswith(link.get('url_suffix')):
+            if url.find(link.get('url_suffix')) < 1:
                 bad_links.append('%s does not end with %s' % (url, link.get('url_suffix')))
         Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
 
@@ -36,7 +36,7 @@ class TestApps:
         bad_links = []
         for link in Apps.Header.tabzilla_links_list:
             url = apps_page.link_destination(link.get('locator'))
-            if not url.endswith(link.get('url_suffix')):
+            if url.find(link.get('url_suffix')) < 1:
                 bad_links.append('%s does not end with %s' % (url, link.get('url_suffix')))
         Assert.equal(0, len(bad_links), '%s bad links found: ' % len(bad_links) + ', '.join(bad_links))
 
