256f00669b5eba859f85c2c4168abcdfd7486125
retornam <etornam.raymond@gmail.com>
2013-11-21 15:26:31 -0800

    Updating locators for new products page

--- pages/desktop/products.py
+++ pages/desktop/products.py
@@ -16,116 +16,118 @@ class ProductsPage(Base):
     product_nav_links_list = [
         {
             'locator': (By.CSS_SELECTOR, '#products-nav li:nth-child(1) a'),
-            'url_suffix': '/products/#products',
+            'url_suffix': '/products/#firefox',
         }, {
             'locator': (By.CSS_SELECTOR, '#products-nav li:nth-child(2) a'),
-            'url_suffix': '/products/#innovations',
+            'url_suffix': '/products/#mozilla',
         }, {
             'locator': (By.CSS_SELECTOR, '#products-nav li:nth-child(3) a'),
-            'url_suffix': '/products/#tools',
-        }, {
-            'locator': (By.CSS_SELECTOR, '#products-nav li:nth-child(4) a'),
-            'url_suffix': '/products/#platforms',
+            'url_suffix': '/products/#developers',
         }
     ]
 
     images_list = [
         {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(1) a img'),
+            'locator': (By.CSS_SELECTOR, '#firefox li:nth-child(1) a img'),
             'img_name_suffix': 'badge-firefox.jpg?2013-06',
         }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(2) a img'),
+            'locator': (By.CSS_SELECTOR, '#firefox li:nth-child(2) a img'),
+            'img_name_suffix': 'badge-android.jpg',
+        }, {
+            'locator': (By.CSS_SELECTOR, '#firefox li:nth-child(3) a img'),
             'img_name_suffix': 'badge-firefoxos.jpg?2013-06',
         }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(3) a img'),
+            'locator': (By.CSS_SELECTOR, '#firefox li:nth-child(4) a img'),
             'img_name_suffix': 'badge-marketplace.jpg?2013-06',
         }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(4) a img'),
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(1) a img'),
             'img_name_suffix': 'badge-persona.jpg',
         }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(5) a img'),
-            'img_name_suffix': 'badge-thunderbird.jpg',
-        }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(6) a img'),
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(2) a img'),
             'img_name_suffix': 'badge-webmaker.jpg',
         }, {
-            'locator': (By.CSS_SELECTOR, '#innovations li:nth-child(1) a img'),
-            'img_name_suffix': 'badge-webfwd.jpg',
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(3) a img'),
+            'img_name_suffix': 'badge-openbadges.png',
         }, {
-            'locator': (By.CSS_SELECTOR, '#innovations li:nth-child(2) a img'),
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(4) a img'),
             'img_name_suffix': 'badge-labs.jpg',
         }, {
-            'locator': (By.CSS_SELECTOR, '#tools li:nth-child(1) a img'),
-            'img_name_suffix': 'badge-tools.jpg',
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(5) a img'),
+            'img_name_suffix': 'badge-webfwd.jpg',
         }, {
-            'locator': (By.CSS_SELECTOR, '#tools li:nth-child(2) a img'),
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(6) a img'),
+            'img_name_suffix': 'badge-thunderbird.jpg',
+        }, {
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(1) a img'),
             'img_name_suffix': 'badge-bugzilla.jpg',
         }, {
-            'locator': (By.CSS_SELECTOR, '#tools li:nth-child(3) a img'),
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(2) a img'),
             'img_name_suffix': 'badge-firebug.jpg',
         }, {
-            'locator': (By.CSS_SELECTOR, '#platforms li:nth-child(1) a img'),
-            'img_name_suffix': 'badge-gecko.jpg',
-        }, {
-            'locator': (By.CSS_SELECTOR, '#platforms li:nth-child(2) a img'),
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(3) a img'),
             'img_name_suffix': 'badge-xulrunner.jpg',
         }, {
-            'locator': (By.CSS_SELECTOR, '#platforms li:nth-child(3) img'),
-            'img_name_suffix': 'badge-reserved.jpg',
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(4) img'),
+            'img_name_suffix': 'badge-gecko.jpg',
+        }, {
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(5) img'),
+            'img_name_suffix': 'badge-api.jpg',
         }
     ]
 
-    products_links_list = [
+    firefox_links_list = [
         {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(1) a'),
-            'url_suffix': '/firefox/',
+            'locator': (By.CSS_SELECTOR, '#firefox li:nth-child(1) a'),
+            'url_suffix': '/firefox/features/',
         }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(2) a'),
+            'locator': (By.CSS_SELECTOR, '#firefox li:nth-child(2) a'),
+            'url_suffix': '/firefox/mobile/features/',
+        }, {
+            'locator': (By.CSS_SELECTOR, '#firefox li:nth-child(3) a'),
             'url_suffix': '/firefox/os/',
         }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(3) a'),
-            'url_suffix': '/apps/',
-        }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(4) a'),
+            'locator': (By.CSS_SELECTOR, '#firefox li:nth-child(4) a'),
+            'url_suffix': 'marketplace.firefox.com/',
+        }
+    ]
+
+    mozilla_links_list = [
+        {
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(1) a'),
             'url_suffix': '/persona/',
         }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(5) a'),
-            'url_suffix': '/thunderbird/',
-        }, {
-            'locator': (By.CSS_SELECTOR, '#products li:nth-child(6) a'),
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(2) a'),
             'url_suffix': 'webmaker.org/',
-        }
-    ]
-
-    innovations_links_list = [
-        {
-            'locator': (By.CSS_SELECTOR, '#innovations li:nth-child(1) a'),
+        }, {
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(3) a'),
+            'url_suffix': 'openbadges.org/',
+        }, {
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(4) a'),
+            'url_suffix': 'mozillalabs.com/',
+        }, {
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(5) a'),
             'url_suffix': 'webfwd.org/',
         }, {
-            'locator': (By.CSS_SELECTOR, '#innovations li:nth-child(2) a'),
-            'url_suffix': 'mozillalabs.com/',
+            'locator': (By.CSS_SELECTOR, '#mozilla li:nth-child(6) a'),
+            'url_suffix': '/thunderbird/',
         }
     ]
 
-    tools_links_list = [
+    developers_links_list = [
         {
-            'locator': (By.CSS_SELECTOR, '#tools li:nth-child(1) a'),
-            'url_suffix': '/Tools/',
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(1) a'),
+            'url_suffix': 'bugzilla.org/',
         }, {
-            'locator': (By.CSS_SELECTOR, '#tools li:nth-child(2) a'),
-            'url_suffix': 'www.bugzilla.org/',
-        }, {
-            'locator': (By.CSS_SELECTOR, '#tools li:nth-child(3) a'),
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(2) a'),
             'url_suffix': 'getfirebug.com/',
-        }
-    ]
-
-    platforms_links_list = [
-        {
-            'locator': (By.CSS_SELECTOR, '#platforms li:nth-child(1) a'),
-            'url_suffix': '/Gecko',
         }, {
-            'locator': (By.CSS_SELECTOR, '#platforms li:nth-child(2) a'),
-            'url_suffix': '/XULRunner',
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(3) a'),
+            'url_suffix': 'developer.mozilla.org/XULRunner',
+        }, {
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(4) a'),
+            'url_suffix': 'developer.mozilla.org/Gecko',
+        }, {
+            'locator': (By.CSS_SELECTOR, '#developers li:nth-child(5) a'),
+            'url_suffix': '/Apps/Reference',
         }
     ]
