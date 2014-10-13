35ab9ee91e4db9ac13f0f3a8d4daec2f9a0122fc
Sergey Tupchiy <stupchiy@gmail.com>
2014-10-13 22:06:50 +0200

    removed xfail for signup, now working as expected

--- tests/test_contribute.py
+++ tests/test_contribute.py
@@ -121,11 +121,11 @@ class TestContribute:
         signup_page.go_to_page()
         Assert.true(signup_page.is_sign_up_form_present, 'The sign up form is not present on the page.')
 
-    @pytest.mark.xfail(reason='Due to error: We apologize, but an error occurred in our system. Please try again later.')
     def test_sign_up_with_valid_email(self, mozwebqa):
         valid_email = 'noreply@mozilla.com'
         country = 'us'
         name = 'mozilla'
+        success_url_slug = 'thankyou'
         contribute_page = Contribute(mozwebqa)
         contribute_page.go_to_page()
         signup_page = contribute_page.click_signup()
@@ -137,7 +137,6 @@ class TestContribute:
         signup_page.select_html_format()
         signup_page.check_privacy_checkbox()
         signup_page.submit_form()
-        success_url_slug = 'sign-up-for-mozilla'
         Assert.true(success_url_slug in signup_page.url_current_page,
                     'Expected current URL slug to be %s, but was not found in %s.' %
                     (success_url_slug, signup_page.url_current_page))
