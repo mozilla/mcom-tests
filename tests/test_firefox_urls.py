63b6d12a9bd67a2b4311abe70db0e411ca692447
Kohei Yoshino <kohei.yoshino@gmail.com>
2015-05-07 10:14:04 -0400

    Add tests for Bug 1110927 - 301 redirect legacy pages that are 404ing

--- tests/test_firefox_urls.py
+++ tests/test_firefox_urls.py
@@ -18,7 +18,7 @@ class TestFirefoxURLs(object):
         '/firefox/brand/',
         '/firefox/channel/',
         '/firefox/channel/android/',
-        '/firefox/desktop//',
+        '/firefox/desktop/',
         '/firefox/developer/',
         '/firefox/geolocation/',
         '/firefox/installer-help/',
@@ -37,11 +37,16 @@ class TestFirefoxURLs(object):
         '/firefox/tiles/',
         '/firefox/unsupported-systems/',
         '/firefox/unsupported/EOL/',
+        # Legacy URLs (Bug 1110927)
+        '/firefox/panorama/',
+        '/firefox/start/central.html',
+        '/firefox/sync/firstrun.html',
     ]
 
     def test_urls_ok(self, mozwebqa):
         for path in self.paths:
             url = mozwebqa.base_url + '/en-US' + path
             response = requests.get(url, timeout=3)
+            assert len(response.history) <= 2
             assert response.status_code == 200, 'got status {} from {}'.format(
                 response.status_code, url)
