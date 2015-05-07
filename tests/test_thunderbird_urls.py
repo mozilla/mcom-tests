76abc116301b159d17fdfc369ec41397fae69772
Kohei Yoshino <kohei.yoshino@gmail.com>
2015-05-07 14:38:13 -0400

    Add tests for Bug 1123399 - Move Thunderbird all download page to Bedrock

---
+++ tests/test_thunderbird_urls.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import pytest
+import requests
+
+
+@pytest.mark.skip_selenium
+@pytest.mark.nondestructive
+class TestThunerbirdURLs(object):
+
+    paths = [
+        '/thunderbird/all/',
+        '/thunderbird/releases/',
+    ]
+
+    def test_urls_ok(self, mozwebqa):
+        for path in self.paths:
+            url = mozwebqa.base_url + '/en-US' + path
+            response = requests.get(url, timeout=3)
+            assert len(response.history) <= 2
+            assert response.status_code == 200, 'got status {} from {}'.format(
+                response.status_code, url)
