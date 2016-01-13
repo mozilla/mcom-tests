60048bc38c4a5549d9cc92ffe0605a7175f5855d
Dave Hunt <dave.hunt@gmail.com>
2016-01-13 12:46:45 +0000

    Remove partnerships tests and remaining references to Selenium based tests

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,6 @@ RUN apt-get update && \
     rm -rf /var/lib/apt/lists/*
 
 ENV BASE_URL "https://www-dev.allizom.org"
-ENV BROWSER_NAME Firefox
-ENV BROWSER_VERSION 39.0
-ENV PLATFORM "Windows 7"
-ENV SELENIUM_VERSION 2.47.1
-ENV BUILD_TAG docker
 ENV PYTHONDONTWRITEBYTECODE=1
 
 COPY requirements.txt /app/requirements.txt
--- run.sh
+++ run.sh
@@ -2,13 +2,6 @@
 
 py.test -r=fsxXR --verbose -n 15 \
         --base-url ${BASE_URL} \
-        --driver SauceLabs \
-        --capability browserName "${BROWSER_NAME}" \
-        --capability version "${BROWSER_VERSION}" \
-        --capability platform "${PLATFORM}" \
-        --capability selenium-version "${SELENIUM_VERSION}" \
-        --capability build "${BUILD_TAG}" \
-        --capability screenResolution "1280x1024" \
         --html results/index.html \
         --junitxml results/results.xml \
         tests
